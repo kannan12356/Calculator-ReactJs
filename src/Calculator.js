a6b365337cfe088290ef9b2962930af967eb9bc6
Kannan <kannanaa21@gmail.com>
2021-12-03 14:35:22 +0530

    Add delete and clear button

--- src/Calculator.js
+++ src/Calculator.js
@@ -33,33 +33,32 @@ function Calculator() {
                 <input type="text" className="input-box" id="result" value={input} readOnly />
                 <div className="button">
                     <div className="first-row">                    
-                        <button className="btn" onClick={()=>setInput('')}>AC</button>
-                        <button className="btn" onClick={()=>setInput(input.substr(0,input.length-1))}>Delete</button>                    
-                        <button className="btn" onClick={()=>inputValue('%')}>%</button>
-                        <button className="btn calc" onClick={()=>inputValue('/')}>/</button>
-                    </div>
-                    <div className="second-row">                    
                         <button className="btn" onClick={()=>inputValue('7')}>7</button>
                         <button className="btn" onClick={()=>inputValue('8')}>8</button>                    
                         <button className="btn" onClick={()=>inputValue('9')}>9</button>
-                        <button className="btn calc" onClick={()=>inputValue('*')}>X</button>
+                        <button className="btn calc" onClick={()=>inputValue('/')}>/</button>
                     </div>
-                    <div className="third-row">                    
+                    <div className="second-row">                    
                         <button className="btn" onClick={()=>inputValue('4')}>4</button>
                         <button className="btn" onClick={()=>inputValue('5')}>5</button>                    
                         <button className="btn" onClick={()=>inputValue('6')}>6</button>
-                        <button className="btn calc" onClick={()=>inputValue('-')}>_</button>
+                        <button className="btn calc" onClick={()=>inputValue('*')}>X</button>
                     </div>
-                    <div className="forth-row">                    
+                    <div className="third-row">                    
                         <button className="btn" onClick={()=>inputValue('1')}>1</button>
                         <button className="btn" onClick={()=>inputValue('2')}>2</button>                    
                         <button className="btn" onClick={()=>inputValue('3')}>3</button>
                         <button className="btn calc" onClick={()=>inputValue('+')}>+</button>
                     </div>
-                    <div className="fifth-row">                    
-                        <button className="btn" onClick={()=>inputValue('0')} style={{width: "170px"}}>0</button>
+                    <div className="forth-row">                    
+                        <button className="btn" onClick={()=>inputValue('0')}>0</button>
                         <button className="btn" onClick={()=>inputValue('.')}>.</button>                    
-                        <button className="btn calc" onClick={sum}>=</button>
+                        <button className="btn" onClick={sum}>=</button>
+                        <button className="btn calc" onClick={()=>inputValue('-')}>-</button>
+                    </div>
+                    <div className="fifth-row">                    
+                        <button className="clr" onClick={()=>setInput('')}>Clear</button>
+                        <button className="clr" onClick={()=>setInput(input.substr(0,input.length-1))}>Delete</button>
                     </div>
                 </div>
             </div>            
